26ec5cc1a6c18acdb3411ad611efdf0283ede0f8
INGcoderdojoMicrobit <71919260+INGcoderdojoMicrobit@users.noreply.github.com>
2021-02-06 10:58:03 +0100

    -Moja mapeczka

--- main.ts
+++ main.ts
@@ -3,11 +3,11 @@ namespace SpriteKind {
 }
 function setLevel (lvl: number) {
     if (lvl == 0) {
-        tiles.setTilemap(tilemap`level1`)
+        tiles.setTilemap(tilemap`level_Michal`)
     } else if (lvl == 1) {
         tiles.setTilemap(tilemap`level2`)
     } else if (lvl == 2) {
-        tiles.setTilemap(tilemap`level0`)
+        tiles.setTilemap(tilemap`level1`)
     }
 }
 function create_wroga () {
